e73c7eac28d7522a313ae219cc53721e31eeb910
Pragnesh Kumar Pallaprolu <pragnesh.pallaprolu@uni-bielefeld.de>
2025-08-31 22:50:33 +0200

    Upload New File

---
+++ id_map.py
@@ -0,0 +1,30 @@
+# --- Edit these if your map changes ---
+PLATE_DISPENSER = "a89cbefdfa6f4a6884817e1df87efe31"
+SERVING_WINDOW  = "c77130acd2da42bf914c594cec077409"
+
+CUTTING_BOARDS  = [
+    "76470b7584da41aa8b135ad10d2438d2",
+    "165b925c66474e3cbe4275f7bebb3b66"
+]
+
+# ingredient dispensers
+BUN     = "0b40aa845294447fb0facce01f052ad3"
+MEAT    = "9541ab457e5a42a5b903e67c88c48061"
+TOMATO  = "9e625c27242843e38192692aa081fe9b"
+LETTUCE = "ce339878689f4305be5c4ee1ce658333"
+ONION   = "e617755152274714b4d8aafe7ce2dd71"   # for OnionSoup
+POTATO  = "e295afc42aec40199a44a677ba7787d6"   # for Chips
+FISH    = "828616225ba0476bb80a74e44359eb2a"   # for FriedFish
+DOUGH   = "d6b0ed2777bd416b8f8fb82d9ad48ac3"   # for Pizza
+CHEESE  = "52b618af5ca64fac96b4df59f6a8822b"
+SAUSAGE = "8625c5f4a1a64819aad0f84179c18a31"
+
+# stoves — hard-code (flip if wrong)
+POT_STOVE = "ae0a29ced6724533a475e0f380657b22"  # tomato/onion soups
+PAN_STOVE = "851e41f2712643b6a2c35e1501039a92"  # burger patty
+
+# fryer & oven
+# DEEP_FRYER = "e295afc42aec40199a44a677ba7787d6"   # main fryer counter
+# # OVEN       = "<fill_this_in>"   # oven counter
+# # PEEL       = "<fill_this_in>"   # peel resting place (holds pizza before baking)
+# BASKET     = "e295afc42aec40199a44a677ba7787d6"   # fryer basket (if static)
